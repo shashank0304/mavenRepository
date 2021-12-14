2fd6daaafa745702dfcde9f6b6b019bea2e7572c
shashank0304 <shashank0304@gmail.com>
2021-12-14 07:49:32 +0530

    Maven Examples for my reference

---
+++ mb2g-surefireplugin/mb2g-surefireplugin/src/test/java/com/ssangu/test/JavaHelloWorldIT.java
@@ -0,0 +1,12 @@
+package com.ssangu.test;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+	
+	@Test
+	public void testJavaHelloWorldIT() {
+		
+		System.out.println("Hello World IT");
+	}
+}
